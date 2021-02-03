dfee9cb96eda9f0f046f85fa4d7d4998984ed00a
mireille1999 <muhawenimana920@daviscollege.com>
2021-02-03 15:28:58 +0200

    created form

--- js/scripts.js
+++ js/scripts.js
@@ -3,61 +3,29 @@ function getSizeCost() {
     var selectedSize = document.getElementById("size").value;
     return parseInt(selectedSize);
 }
-function getCrustCost() {
-    var selectedCrust = document.getElementById("crust").value;
+function getMaterialCost() {
+    var selectedCrust = document.getElementById("materials").value;
     return parseInt(selectedCrust);
 }
 function getNumber() {
-    var selectedNumber = document.getElementById("numberofpizza").value;
+    var selectedNumber = document.getElementById("numberofcleaners").value;
     return parseInt(selectedNumber);
 }
-function mushrooms() {
+function cleaning() {
     var mushroom = 0;
-    var addMushroom = document.getElementById("mush",);
+    var addMushroom = document.getElementById("clean",);
     if (addMushroom.checked === true) {
-        mushroom = 1;
+        mushroom = 300;
     }
     return parseInt(mushroom);
 }
+
 function calctotalPrice(e) {
     event.preventDefault();
-    var totalPrice = (getSizeCost() + getCrustCost() + mushrooms()) * (getNumber());
-    
-        console.log(totalPrice);
-
-    alert("Your resquest of cleaning  " + getNumber() + "  has been processed.Your total amount payable is " + totalPrice +"     Rwf." )
-}
-$(document).ready(function(){
-    $("#delivery").click(function(){
-        $("#delivery").toggle(1000);
-        $("#deliveryForm").toggle(1200);
-        $("#cancel").slideDown(1000);
-    });
-    $("#cancel").click(function(){
-        $("#cancel").slideUp(1000);
-        $("#deliveryForm").slideUp(1200);
-        alert("The delivery is  cancelled.");
-        $("#delivery").slideDown(1200);
-    });
-});
-$(document).ready(function(){
-    $("#deli").click(function(){
-        $("#deli").toggle(1000);
-        $("#mimi").toggle(1200);
-        $("#cancel").slideDown(1000);
-    });
-    $("#cancel").click(function(){
-        $("#cancel").slideUp(1000);
-        $("#deli").slideUp(1200);
-        alert("The delivery is  cancelled.");
-        $("#deli").slideDown(1200);
-    });
-});
-$(document).ready(function () {
-    $("#delivery-info").submit(function () {
-        var name = $("input#name").val();
+    var totalPrice = (getSizeCost() + getMaterialCost() + cleaning()) * (getNumber());
+    var name = $("input#name").val();
         var number = $("input#number").val();
         var location = $("input#location").val();
-        alert("Hello " + name + ". Thank you for chosing Mimi's Pizzeria . Your order has been successfully received and will be delivered to " +  location + ".The delivery will cost 1 $. We will call you shortly on " + number);
-    }); 
-});
+        console.log(totalPrice);
+        alert("Hello " + name + ". Thank you for chosing KFMP Cleaners Ltd. Your request of " + getNumber() + " Cleaner(s) has been processed successfully and  we will be sending cleaner(s) to " +  location +  " . " + "Your total amount payable is "  + totalPrice + ". We will call you shortly on " + number);
+}
